38c409c75d606de02e13557ced6267f6721d793d
Heitor Kobayashi <heitor.kobayashi@gmail.com>
2025-04-12 12:55:49 -0300

    Alteração scripts

--- projeto/ecommerce/processamento_de_vendas.sh
+++ projeto/ecommerce/processamento_de_vendas.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-#Definição do diretório para realização do script
 cd /home/heitorkobayashi/ecommerce
 
-#Criação da pasta vendas
 mkdir -p /home/heitorkobayashi/ecommerce/vendas
 
-#Cópia do arquivo .csv para pasta vendas e mudança de diretório
 cp dados_de_vendas.csv /home/heitorkobayashi/ecommerce/vendas
 cd /home/heitorkobayashi/ecommerce/vendas
 
-#Criação do subdiretório backup
 mkdir -p /home/heitorkobayashi/ecommerce/vendas/backup
 
-#Renomeação do aquivo de backup com base na data do sistema
 data=$(date +%Y%m%d)
 cp dados_de_vendas.csv /home/heitorkobayashi/ecommerce/vendas/backup
 cd /home/heitorkobayashi/ecommerce/vendas/backup
 mv dados_de_vendas.csv backup-dados-$data.csv
 
-#Definição da variável do arquivo backup .csv
 dados_tabela="backup-dados-$data.csv"
 
 #Extração de dados da tabela
@@ -37,11 +31,10 @@ Data e hora da geração do relatório: $(date +'%Y%m%d %Hh:%Mm')
 $(tail -n +2 backup-dados-$data.csv | head -n 10)
 END
 
-#Mensagem final
 echo "O arquivo '$arquivo' foi gerado."
 cat /home/heitorkobayashi/ecommerce/vendas/backup/relatorio-$data.txt
 
-#Compressão e remoção dos arquivos solicitados
+#Compressão
 cd /home/heitorkobayashi/ecommerce/vendas/backup
 zip -r backup-dados-$data.zip backup-dados-$data.csv
 rm backup-dados-$data.csv
